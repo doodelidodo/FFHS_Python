cb87c83f5353799a8bb1373988aa7e7ed5860c94
doodelidodo <dominic.meyer@me.com>
2022-08-28 11:26:09 +0200

    Initial commit

---
+++ dataStructures.py
@@ -0,0 +1,11 @@
+#normales Tuple
+tup = 4, 5, 6
+print(tup)
+
+#Tuple in Tuple
+nested_tup = (2, 3, 4), (5, 6)
+print(nested_tup)
+
+my_string = "Dominic"
+new_tup = (tuple(my_string))
+print(new_tup[0])
---
+++ dateAndSo.py
@@ -0,0 +1,17 @@
+from datetime import datetime, date, time
+
+
+dt = datetime(2011, 10, 29, 20, 30, 21)
+print(dt)
+
+print(dt.date())
+
+new_time = dt.strftime('%d.%m.%Y %H:%M')
+print(new_time)
+
+dt2 = datetime(2011, 11, 15, 22, 30)
+
+delta = dt2 - dt
+print(delta)
+print(type(delta))
+
